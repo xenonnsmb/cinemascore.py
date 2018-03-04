30fd5308d5df762b0f37e462a604355ae5f7bfa4
XenonNSMB <11357889+xenonnsmb@users.noreply.github.com>
2018-03-04 11:03:09 -0600

    better setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     version='0.1',
     packages=['cinemascore',],
     license='MIT License',
+    summary='A tiny Python library for getting Cinemascore movie grades.',
+    home-page='https://github.com/xenonnsmb/cinemascore.py',
     long_description=open('README.md').read(),
     install_requires=[
         "requests >= 2.18.4",
